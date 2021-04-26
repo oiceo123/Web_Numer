971d3ef2ab7fc553266b7c6458615796eaadf895
oiceo123 <iceome44@gmail.com>
2021-04-26 23:19:00 +0700

    Update

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,21 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+var expect = require('expect.js');
+
+function add (a, b) { return a + b; };
+
 test('renders learn react link', () => {
   render(<App />);
-/*   const linkElement = screen.getByText(/learn react/i);
+  /* const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument(); */
+});
+
+it('should do math', function () {
+  expect(console.log(add(1, 3)))
+  expect(add(1, 3)).to.equal(4);
+});
+
+it('fail', function () {
+  expect(add(1, 3)).to.equal(10);
 });

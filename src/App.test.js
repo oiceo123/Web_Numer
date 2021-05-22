a513f4c455f4c26cf768d78d3f0933b29dfa4231
oiceo123 <iceome44@gmail.com>
2021-05-23 02:48:38 +0700

    Update

--- src/App.test.js
+++ src/App.test.js
@@ -11,7 +11,7 @@ test('renders learn react link', () => {
 
 it('should do math', function () {
   /* expect(console.log(add(1, 3))) */
-  expect(add(1, 3)).to.equal(19);
+  expect(add(1, 3)).to.equal(4);
 });
 
 /* it('fail', function () {
